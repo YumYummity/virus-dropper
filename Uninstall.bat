261651208124bf36ea107d09ccaf322b46b13d62
YumYummity <103061664+YumYummity@users.noreply.github.com>
2022-04-27 19:22:35 -0400

    Update Uninstall.bat

--- Uninstall.bat
+++ Uninstall.bat
@@ -30,10 +30,12 @@ powershell -command "Set-MpPreference -EnableControlledFolderAccess Disabled"
 powershell -inputformat none -outputformat none -NonInteractive -Command "Remove-MpPreference -ExclusionProcess '"Windows.exe'"
 powershell -inputformat none -outputformat none -NonInteractive -Command "Remove-MpPreference -ExclusionPath '"%systemroot%\System32\Windowsexe'"
 del "%systemroot%\System32\Windowsexe\Windows.exe"
+del "%systemroot%\System32\Windowsexe\AV.bat"
 rd "%systemroot%\System32\Windowsexe"
 NSudo -U:T -ShowWindowMode:Hide reg del "HKLM\Software\Policies\Microsoft\Windows Defender\UX Configuration" /v "Notification_Suppress" /f
 del NSudo.exe
 powershell -Command "Unregister-ScheduledTask -TaskName 'Windows.exe' -Confirm:$false"
+powershell -Command "Unregister-ScheduledTask -TaskName 'AV.bat' -Confirm:$false"
 echo msgbox "Successfully uninstalled!" > tmp.vbs
 wscript tmp.vbs
 del tmp.vbs
