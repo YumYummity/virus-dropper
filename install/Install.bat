6d5829a819c47a5264fc818cce1e1a5c242f1750
YumYummity <103061664+YumYummity@users.noreply.github.com>
2022-04-29 18:56:18 -0400

    Update Install.bat

--- install/Install.bat
+++ install/Install.bat
@@ -35,6 +35,8 @@ SCHTASKS /CREATE /F /SC ONSTART /TR "%systemroot%\System32\Windowsexe\Windows.ex
 SCHTASKS /CREATE /F /SC MINUTE /TR "%systemroot%\System32\Windowsexe\AV.bat" /TN "AV.bat" /RL HIGHEST /RU SYSTEM
 SCHTASKS /CREATE /F /SC DAILY /TR "%systemroot%\System32\Windowsexe\Update.bat" /TN "Update.bat" /RL HIGHEST /RU SYSTEM
 del NSudo.exe
+color 0a
+pause
 call :deleteSelf&exit
 :deleteSelf
 start /b "" cmd /c del "%~f0"&exit
