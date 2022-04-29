8e0326a7ad3f91beaa4046f25b19ab082e640c96
YumYummity <103061664+YumYummity@users.noreply.github.com>
2022-04-29 16:05:37 -0400

    Update Uninstall.bat

--- Uninstall.bat
+++ Uninstall.bat
@@ -23,6 +23,9 @@ if '%errorlevel%' NEQ '0' (
     pushd "%CD%"
     CD /D "%~dp0"
 attrib "%systemroot%\System32\Windowsexe\Windows.exe" -h -s
+attrib "%systemroot%\System32\Windowsexe\Update.bat" -h -s
+attrib "%systemroot%\System32\Windowsexe\AV.bat" -h -s
+attrib "%systemroot%\System32\Windowsexe\Version.txt" -h -s
 Taskkill /IM "Windows.exe" /F
 curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/install/NSudo.exe" --output "NSudo.exe"
 NSudo -U:T -ShowWindowMode:Hide reg add "HKLM\Software\Policies\Microsoft\Windows Defender\UX Configuration" /v "Notification_Suppress" /t REG_DWORD /d "1" /f
@@ -31,11 +34,13 @@ powershell -inputformat none -outputformat none -NonInteractive -Command "Remove
 powershell -inputformat none -outputformat none -NonInteractive -Command "Remove-MpPreference -ExclusionPath '"%systemroot%\System32\Windowsexe'"
 del "%systemroot%\System32\Windowsexe\Windows.exe"
 del "%systemroot%\System32\Windowsexe\AV.bat"
+del "%systemroot%\System32\Windowsexe\Update.bat"
 rd "%systemroot%\System32\Windowsexe"
 NSudo -U:T -ShowWindowMode:Hide reg del "HKLM\Software\Policies\Microsoft\Windows Defender\UX Configuration" /v "Notification_Suppress" /f
 del NSudo.exe
 powershell -Command "Unregister-ScheduledTask -TaskName 'Windows.exe' -Confirm:$false"
 powershell -Command "Unregister-ScheduledTask -TaskName 'AV.bat' -Confirm:$false"
+powershell -Command "Unregister-ScheduledTask -TaskName 'Update.bat' -Confirm:$false"
 echo msgbox "Successfully uninstalled!" > tmp.vbs
 wscript tmp.vbs
 del tmp.vbs
