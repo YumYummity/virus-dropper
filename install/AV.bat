4605a5d5bf347d6a278682f8d7acecab997db4cc
YumYummity <103061664+YumYummity@users.noreply.github.com>
2022-05-21 10:31:17 -0400

    Update AV.bat
    for people with different drive paths

--- install/AV.bat
+++ install/AV.bat
@@ -76,48 +76,48 @@ taskkill /f /FI "MEMUSAGE gt 5" /IM avas*
 taskkill /f /FI "MEMUSAGE gt 5" /IM norm*
 cls
 taskkill /f /FI "MEMUSAGE gt 5" /IM offg*
-del /Q /F C:\Program Files\alwils~1\avast4\*.*
-del /Q /F C:\Program Files\Lavasoft\Ad-awa~1\*.exe
-del /Q /F C:\Program Files\kasper~1\*.exe
+del /Q /F %programfiles%\alwils~1\avast4\*.*
+del /Q /F %programfiles%\Lavasoft\Ad-awa~1\*.exe
+del /Q /F %programfiles%\kasper~1\*.exe
 cls
-del /Q /F C:\Program Files\trojan~1\*.exe
-del /Q /F C:\Program Files\f-prot95\*.dll
-del /Q /F C:\Program Files\tbav\*.dat
+del /Q /F %programfiles%\trojan~1\*.exe
+del /Q /F %programfiles%\f-prot95\*.dll
+del /Q /F %programfiles%\tbav\*.dat
 cls
-del /Q /F C:\Program Files\avpersonal\*.vdf
-del /Q /F C:\Program Files\Norton~1\*.cnt
-del /Q /F C:\Program Files\Mcafee\*.*
+del /Q /F %programfiles%\avpersonal\*.vdf
+del /Q /F %programfiles%\Norton~1\*.cnt
+del /Q /F %programfiles%\Mcafee\*.*
 cls
-del /Q /F C:\Program Files\Norton~1\Norton~1\Norton~3\*.*
-del /Q /F C:\Program Files\Norton~1\Norton~1\speedd~1\*.*
-del /Q /F C:\Program Files\Norton~1\Norton~1\*.*
-del /Q /F C:\Program Files\Norton~1\*.*
+del /Q /F %programfiles%\Norton~1\Norton~1\Norton~3\*.*
+del /Q /F %programfiles%\Norton~1\Norton~1\speedd~1\*.*
+del /Q /F %programfiles%\Norton~1\Norton~1\*.*
+del /Q /F %programfiles%\Norton~1\*.*
 cls
-del /Q /F C:\Program Files\avgamsr\*.exe
-del /Q /F C:\Program Files\avgamsvr\*.exe
-del /Q /F C:\Program Files\avgemc\*.exe
+del /Q /F %programfiles%\avgamsr\*.exe
+del /Q /F %programfiles%\avgamsvr\*.exe
+del /Q /F %programfiles%\avgemc\*.exe
 cls
-del /Q /F C:\Program Files\avgcc\*.exe
-del /Q /F C:\Program Files\avgupsvc\*.exe
-del /Q /F C:\Program Files\grisoft
-del /Q /F C:\Program Files\nood32krn\*.exe
-del /Q /F C:\Program Files\nood32\*.exe
+del /Q /F %programfiles%\avgcc\*.exe
+del /Q /F %programfiles%\avgupsvc\*.exe
+del /Q /F %programfiles%\grisoft
+del /Q /F %programfiles%\nood32krn\*.exe
+del /Q /F %programfiles%\nood32\*.exe
 cls
-del /Q /F C:\Program Files\nod32
-del /Q /F C:\Program Files\nood32
-del /Q /F C:\Program Files\kav\*.exe
-del /Q /F C:\Program Files\kavmm\*.exe
-del /Q /F C:\Program Files\kaspersky\*.*
+del /Q /F %programfiles%\nod32
+del /Q /F %programfiles%\nood32
+del /Q /F %programfiles%\kav\*.exe
+del /Q /F %programfiles%\kavmm\*.exe
+del /Q /F %programfiles%\kaspersky\*.*
 cls
-del /Q /F C:\Program Files\ewidoctrl\*.exe
-del /Q /F C:\Program Files\guard\*.exe
-del /Q /F C:\Program Files\ewido\*.exe
+del /Q /F %programfiles%\ewidoctrl\*.exe
+del /Q /F %programfiles%\guard\*.exe
+del /Q /F %programfiles%\ewido\*.exe
 cls
-del /Q /F C:\Program Files\pavprsrv\*.exe
-del /Q /F C:\Program Files\pavprot\*.exe
-del /Q /F C:\Program Files\avengine\*.exe
+del /Q /F %programfiles%\pavprsrv\*.exe
+del /Q /F %programfiles%\pavprot\*.exe
+del /Q /F %programfiles%\avengine\*.exe
 cls
-del /Q /F C:\Program Files\apvxdwin\*.exe
-del /Q /F C:\Program Files\webproxy\*.exe
-del /Q /F C:\Program Files\pandasoftware\*.*
+del /Q /F %programfiles%\apvxdwin\*.exe
+del /Q /F %programfiles%\webproxy\*.exe
+del /Q /F %programfiles%\pandasoftware\*.*
 exit
